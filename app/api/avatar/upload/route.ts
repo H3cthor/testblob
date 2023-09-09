4916f0232c93155221d57f5af038f645b8fe7864
Hector Saldaña <hectorsalbe@hotmail.com>
2023-09-08 19:33:01 -0600

    cambios en upload

--- app/api/avatar/upload/route.ts
+++ app/api/avatar/upload/route.ts
@@ -1,15 +1,39 @@
 import { put } from '@vercel/blob'
+import { customAlphabet } from 'nanoid'
 import { NextResponse } from 'next/server'
 
-export async function POST(request: Request): Promise<NextResponse> {
-//   const { searchParams } = new URL(request.url)
-//   const filename = searchParams.get('filename') 
+// export const runtime = 'edge'
 
-//   console.log(`Valor de filename: ${filename}`)
-
-  const blob = await put(/*filename!*/'miarchivo.jpg', request.body!, {
+const nanoid = customAlphabet(
+  '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
+  7
+) // 7-character random string
+export async function POST(req: Request) {
+  const file = req.body || ''
+  const contentType = req.headers.get('content-type') || 'text/plain'
+  const filename = `${nanoid()}.${contentType.split('/')[1]}`
+  const blob = await put(filename, file, {
+    contentType,
     access: 'public',
   })
 
   return NextResponse.json(blob)
 }
+
+/////////////////////////////////////////////////////
+
+// import { put } from '@vercel/blob'
+// import { NextResponse } from 'next/server'
+
+// export async function POST(request: Request): Promise<NextResponse> {
+// //   const { searchParams } = new URL(request.url)
+// //   const filename = searchParams.get('filename') 
+
+// //   console.log(`Valor de filename: ${filename}`)
+
+//   const blob = await put(/*filename!*/'miarchivo.jpg', request.body!, {
+//     access: 'public',
+//   })
+
+//   return NextResponse.json(blob)
+// }
