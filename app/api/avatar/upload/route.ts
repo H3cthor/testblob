0fa4970af7b6569be25d09d8826e7626f9c53dce
Hector Saldaña <hectorsalbe@hotmail.com>
2023-09-08 19:53:05 -0600

    cambios en upload

--- app/api/avatar/upload/route.ts
+++ app/api/avatar/upload/route.ts
@@ -1,27 +1,15 @@
-import { put } from '@vercel/blob'
-import { customAlphabet } from 'nanoid'
-import { NextResponse } from 'next/server'
-
-// export const runtime = 'edge'
-
-const nanoid = customAlphabet(
-  '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
-  7
-) // 7-character random string
-export async function POST(req: Request) {
-
-  const file = req.body || ''
-
-  const contentType = req.headers.get('content-type') || 'text/plain'
-
-  const filename = `${nanoid()}.${contentType.split('/')[1]}`
-  
-  const blob = await put(filename, file, {
-    contentType,
+import { put } from '@vercel/blob';
+import { NextResponse } from 'next/server';
+ 
+export async function POST(request: Request): Promise<NextResponse> {
+  const { searchParams } = new URL(request.url);
+  const filename = searchParams.get('filename');
+ 
+  const blob = await put(filename!, request.body!, {
     access: 'public',
-  })
-
-  return NextResponse.json(blob)
+  });
+ 
+  return NextResponse.json(blob);
 }
 
 /////////////////////////////////////////////////////
